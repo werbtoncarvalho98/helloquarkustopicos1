e8ed9278b5c60fdd1a461036dd3f0568e8ff6bad
Werbton Carvalho <84273781+werbtoncarvalho98@users.noreply.github.com>
2023-02-24 20:17:13 -0300

    second commit

--- src/main/java/br/unitins/resource/JogoResource.java
+++ src/main/java/br/unitins/resource/JogoResource.java
@@ -2,9 +2,12 @@ package br.unitins.resource;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import br.unitins.model.Jogo;
@@ -13,13 +16,7 @@ import java.util.List;
 @Path("/Jogo")
 public class JogoResource {
     
-    @GET
-    @Path("/All")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public List<Jogo> getAll() {
-        return Jogo.findAll().list();
-    }
-
+    // Inserir
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
@@ -28,5 +25,53 @@ public class JogoResource {
         jogo.persist();
 
         return jogo;
+    }
+
+    // Alterar
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Jogo updateGame(@PathParam("id") Long id, Jogo jogo) {
+        Jogo atualizado = Jogo.findById(id);
+
+        atualizado.setNome(jogo.getNome());
+        atualizado.setEstudio(jogo.getEstudio());
+        atualizado.setClassificacao(jogo.getClassificacao());
+        atualizado.setValor(jogo.getValor());
+        atualizado.setLancamento(jogo.getLancamento());
+
+        return atualizado;
+    }
+
+    // Deletar
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Jogo deleteJogo(@PathParam("id") Long id, Jogo jogo) {
+        Jogo deletado = Jogo.findById(id);
+        
+        deletado.delete();
+
+        return deletado;
+    }
+
+    // Consultar todos
+    @GET
+    @Path("/All")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public List<Jogo> getAll() {
+        return Jogo.findAll().list();
+    }
+
+    // Consultar por id
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Jogo searchJogo(@PathParam("id") Long id) {
+        return Jogo.findById(id);
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,10 @@
 -- The commands are commented as their support depends of the database
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+-- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');]
+
+insert into jogo (id, nome, estudio, classificacao, valor, lancamento) values(nextval('hibernate_sequence'), 'God of War', 'Sony', 18, 300, '2018-04-20');
+insert into jogo (id, nome, estudio, classificacao, valor, lancamento) values(nextval('hibernate_sequence'), 'Sonic Frontiers', 'SEGA', 13, 200, '2022-10-08');
+insert into jogo (id, nome, estudio, classificacao, valor, lancamento) values(nextval('hibernate_sequence'), 'Atomic Heart', 'Focus Entertainment', 18, 200, '2023-02-21');
+insert into jogo (id, nome, estudio, classificacao, valor, lancamento) values(nextval('hibernate_sequence'), 'Zelda BOTW', 'Nintendo', 12, 300, '2017-03-03');
+insert into jogo (id, nome, estudio, classificacao, valor, lancamento) values(nextval('hibernate_sequence'), 'Super Mario Odyssey', 'Nintendo', 12, 300, '2017-09-27');
